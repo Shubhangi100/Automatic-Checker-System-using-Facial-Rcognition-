bed3671b29e76ba175e429c17e7535f70888f414
Devesh Pandey <pandeydevesh1296@gmail.com>
2018-09-06 19:54:42 +0530

    Update Moving_Files.py

--- Python/Moving_Files.py
+++ Python/Moving_Files.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np 
 import os
 import shutil
-
-path= 'F:\\Dev\\MLprojects\\New folder'
+## Moving files for preprocessing purpose
+path= ''
 src_files= os.listdir(path)
 num=1
 for files in src_files:
 	src_file= os.path.join(path,files)
 	x= files.split('-')[-2]
-	dest_file= f'F:\\Dev\\MLprojects\\test_dir\\{x}\\{files}'
+	dest_file= f''
 	shutil.move(src_file, dest_file)
 	print(num)
 	num+=1
